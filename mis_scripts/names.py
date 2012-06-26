89811a2d4874d36374046509e2315cdbb1794489
Arjuna <arjunasuresh3@gmail.com>
2012-06-26 12:12:33 +0530

    changed file name

--- mis_scripts/names.py
+++ mis_scripts/names.py
@@ -4,7 +4,7 @@ min = 5
 max = 15
 total = 10000000
 string=''
-FILE = open("filename.out","w")
+FILE = open("names.txt","w")
 for count in xrange(1,total):
   for x in random.sample(alphabet,random.randint(min,max)):
       string+=x
